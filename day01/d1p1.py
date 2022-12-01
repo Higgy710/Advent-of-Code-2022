0e1075851abdaa2a865427df791714cbe08f40f1
Dan Higginbotham <Danzanzio@gmail.com>
2022-12-01 08:43:06 -0600

    Update README.md

--- day01/d1p1.py
+++ day01/d1p1.py
@@ -42,15 +42,16 @@
 with open('input.txt') as f:
     data = f.read()
     data = data.splitlines()
-    calories = []
-    total_calories = 0
-    for line in data:
-        if line == '':
-            calories.append(total_calories)
-            total_calories = 0
-        else:
-            total_calories += int(line)
-    calories.append(total_calories)
 
+calories = []
+total_calories = 0
 
+for line in data:
+    if line == '':
+        calories.append(total_calories)
+        total_calories = 0
+    else:
+        total_calories += int(line)
+
+calories.append(total_calories)
 print('Max calories:',max(calories))
